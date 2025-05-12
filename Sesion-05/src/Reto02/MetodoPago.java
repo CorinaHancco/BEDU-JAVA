298b92884bf3669d718370c756fda1d66816386c
Corina Hancco Vargas <chanccov@unsa.edu.pe>
2025-05-11 21:02:48 -0500

    Reto 2 de la sesion 5 terminada

---
+++ Sesion-05/src/Reto02/MetodoPago.java
@@ -0,0 +1,15 @@
+package Reto02;
+
+public abstract class MetodoPago {
+    protected double monto;
+
+    public MetodoPago(double monto) {
+        this.monto = monto;
+    }
+
+    public abstract void procesarPago();
+
+    public void mostrarResumen() {
+        System.out.println("📄 Tipo: " + this.getClass().getSimpleName() + " - Monto: $" + monto);
+    }
+}
